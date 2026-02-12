78eb56b899869b7488a3cd04f1397d7055492f02
kynexi <184446386+kynexi@users.noreply.github.com>
2026-02-12 14:35:32 +0200

    Add generate strings functionality

--- 1_RegularGrammars/src/Grammar.java
+++ 1_RegularGrammars/src/Grammar.java
@@ -15,4 +15,26 @@ class Grammar {
         this.productions = productions;
         this.startSymbol = startSymbol;
     }
+
+    public String generateString() {
+        StringBuilder result = new StringBuilder();
+        String current = startSymbol;
+        Random random = new Random();
+
+        while (true) {
+            List<String> rules = productions.get(current);
+            String production = rules.get(random.nextInt(rules.size()));
+
+            if (production.length() == 1) {
+                result.append(production);
+                break;
+            } else {
+                result.append(production.charAt(0));
+                current = String.valueOf(production.charAt(1));
+            }
+        }
+
+        return result.toString();
     }
+
+}

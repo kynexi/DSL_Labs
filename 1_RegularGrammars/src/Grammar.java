29122a5900675164b74e5eca89f5e3b42c08d604
kynexi <184446386+kynexi@users.noreply.github.com>
2026-02-12 14:34:30 +0200

    Add grammar class constructor and variables

--- 1_RegularGrammars/src/Grammar.java
+++ 1_RegularGrammars/src/Grammar.java
@@ -1,2 +1,18 @@
-public class Grammar {
-}
+import java.util.*;
+
+class Grammar {
+
+    Set<String> VN;
+    Set<String> VT;
+    Map<String, List<String>> productions;
+    String startSymbol;
+
+    public Grammar(Set<String> VN, Set<String> VT,
+                   Map<String, List<String>> productions,
+                   String startSymbol) {
+        this.VN = VN;
+        this.VT = VT;
+        this.productions = productions;
+        this.startSymbol = startSymbol;
+    }
+    }

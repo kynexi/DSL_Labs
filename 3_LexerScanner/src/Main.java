d14f318e2e934d6d2254d6861117c8d00042c68f
kynexi <184446386+kynexi@users.noreply.github.com>
2026-03-11 02:20:44 +0200

    Add testing of lexer using input and output files

--- 3_LexerScanner/src/Main.java
+++ 3_LexerScanner/src/Main.java
@@ -1,5 +1,30 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) throws Exception {
+        String code = Files.readString(Paths.get("3_LexerScanner/src/input.txt"));
+
+        Tokenizer tokenizer = new Tokenizer(code);
+        List<String> lexemes = tokenizer.tokenize();
+
+        Lexer lexer = new Lexer();
+        List<Token> tokens = lexer.analyze(lexemes);
+
+        StringBuilder output = new StringBuilder();
+
+        output.append("Lexemes:\n");
+        for (String lex : lexemes) {
+            output.append(lex).append("\n");
+        }
+
+        output.append("\nTokens:\n");
+        for (Token token : tokens) {
+            output.append(token).append("\n");
+        }
+
+        Files.writeString(Paths.get("3_LexerScanner/src/output.txt"), output.toString());
     }
 }
